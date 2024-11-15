4ab0c8a3aa33febb729d62f29e0d7c0780dcb1a6
sithi356 <faria35629@gmail.com>
2024-11-15 18:58:47 +0600

    updated notifyRealTime by sithi

--- app/src/main/java/com/jusixs/ndrs/data/repository/AlertRepository.java
+++ app/src/main/java/com/jusixs/ndrs/data/repository/AlertRepository.java
@@ -4,9 +4,8 @@ import com.jusixs.ndrs.data.model.Alert;
 
 public class AlertRepository {
     public Alert sendAlert(Alert alert) {
-        // Here you would perform the network call or database operation.
-        // For the purpose of TDD, we will mock this in tests.
-        alert.setStatus("sent"); // Assuming the alert is successfully sent.
+
+        alert.setStatus("sent");
         return alert;
     }
 }
