028f24f0622ffbe404a4c1864418fe6239b7dbaa
musfikussalihinsifat <musfikus.stu2019@juniv.edu>
2024-11-15 18:05:14 +0600

    Javadoc added

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.ndrsshelterinformationsifat"},{"l":"com.example.ndrsshelterinformationsifat.data.model"},{"l":"com.example.ndrsshelterinformationsifat.ui.view"},{"l":"com.example.ndrsshelterinformationsifat.ui.viewmodel"}];updateSearchResults();
