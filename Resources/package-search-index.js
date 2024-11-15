8a8e061479f3362d9a69841c7ceecad8fd46ba68
nasrin2211 <nasrin.stu2018@juniv.edu>
2024-11-15 23:07:35 +0600

    javadoc final files generated

---
+++ Resources/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jusixs.ndrs"},{"l":"com.jusixs.ndrs.data.model"},{"l":"com.jusixs.ndrs.data.repository"},{"l":"com.jusixs.ndrs.ui.viewmodel"}];updateSearchResults();
